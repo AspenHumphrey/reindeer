5e5ca60ec8fc1fa0f54569689bbe4f842b9fec2a
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-08 11:48:07 -0500

    got loop to layout perfectly in console. working on displaying to html with document.blah blah and .appendChild....its all printing to the dom but not 2 and 2 as in blue dasher in one line

--- reindeer.js
+++ reindeer.js
@@ -6,11 +6,34 @@ var reindeerColor = reindeer.length;
 
 for (var i = 0; i < reindeerColor; i++) {
 	console.log(`${reindeer[i]} ${colors[i]}`  );
+
+	//document.write(`${reindeer[i]} ${colors[i]}`);
+
+	var currentReindeer = reindeer[i];
+	var currentColor = colors[i];
+
+	hohoho(reindeerColor, currentColor)
+
 }
 
+function hohoho(reindeerColor, currentColor) {
+	var hohohoElement = document.getElementById("reindeer");
+	var hohohoElementDiv = document.createElement("div");
+	var	hohohoElementNode = document.createTextNode(currentReindeer);
 
 
-var hohohoElement = document.getElementById("reindeer");
+	var hoElement = document.getElementById("reindeer");
+	var hoElementDiv = document.createElement("div");
+	var hoElementNode = document.createTextNode(currentColor);
+
+	hohohoElementDiv.appendChild(hohohoElementNode);
+	hohohoElement.appendChild(hohohoElementDiv);
+
+	hoElementDiv.appendChild(hoElementNode);
+	hohohoElement.appendChild(hoElementDiv);
+
+}
+
 
 
 
