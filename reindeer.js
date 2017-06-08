f81f008af79d09832e07547083e5e4bd244b48a6
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-08 11:19:00 -0500

    created a loop that passes through both arrays console.logging the corresponding color index with reindeer index works great

---
+++ reindeer.js
@@ -0,0 +1,46 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+
+var reindeerColor = reindeer.length;
+
+for (var i = 0; i < reindeerColor; i++) {
+	console.log(`${reindeer[i]} ${colors[i]}`  );
+}
+
+
+
+var hohohoElement = document.getElementById("reindeer");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
